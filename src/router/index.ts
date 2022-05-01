87567c76acc1c8b8bc51cc1414544dde3c2079e1
dss285 <aeonmoans@gmail.com>
2022-05-01 16:54:45 +0300

    Webhistory to webhashhistory

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import ArknightsDropsView from "../views/ArknightsDropsView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: "/",
